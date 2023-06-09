c609e5cd8de82c49563c1eb56e560af0e9054ca7
LS-Borges <73033006+LS-Borges@users.noreply.github.com>
2023-06-09 10:37:19 -0700

    Initial commit

---
+++ PSPApplication/P2PClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace P2PApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new FileSharingClient();
+            client.Connect("127.0.0.1", 8000);
+
+            client.SendFile("sampleFile.txt");
+            client.Disconnect();
+        }
+    }
+
+    class FileSharingClient
+    {
+        TcpClient client;
+
+        public void Connect(string serverIP, int serverPort)
+        {
+            client = new TcpClient();
+            client.Connect(serverIP, serverPort);
+
+            Console.WriteLine("Connected to the Server");
+        }
+
+        public void SendFile(string path)
+        {
+            var fileData = File.ReadAllBytes(path);
+            var stream = client.GetStream();
+
+            stream.Write(fileData, 0 , fileData.Length);
+            Console.WriteLine("File sent to the server");
+
+            stream.Close();
+        }
+
+        public void Disconnect()
+        {
+            client.Close();
+            Console.WriteLine("Disconnected by client");
+        }
+    }
+}
---
+++ PSPApplication/PSPApplication/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace P2PApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new FileSharingServer();
+            server.Start();
+        }
+    }
+    class FileSharingServer
+    {
+        TcpListener listener;
+
+        public void Start()
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+            listener.Start();
+
+            Console.WriteLine("Waiting to receive files...");
+
+            var clientThread = new Thread(AcceptConnections);
+            clientThread.Start();
+        }
+
+        public void AcceptConnections()
+        {
+            while(true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+
+                var stream = client.GetStream();
+                byte[] fileData = new byte[1024];
+
+                stream.Read(fileData);
+
+                int receivedBytes = stream.Read(fileData, 0, fileData.Length);
+                using (var fs = File.Create("receivedFile.txt"))
+                {
+                    fs.Write(fileData, 0, receivedBytes);
+                }
+                Console.WriteLine("File received from the Client");
+            }
+        }
+    }
+}
